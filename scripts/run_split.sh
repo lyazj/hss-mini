91aaaffc0a2d5676921bea149c376424c8d96454
lyazj <seeson@pku.edu.cn>
2024-01-14 15:19:11 +0100

    Move 4f -> 6f.

--- scripts/run_split.sh
+++ scripts/run_split.sh
@@ -5,8 +5,8 @@ if [ $# != 3 ]; then
     exit 1
 fi
 
-# Current configuration: Z -> qq, 4 flavours
-for PID in 1 2 3 4; do
+# Current configuration: Z -> qq, 6 flavours
+for PID in 1 2 3 4 5 6; do
     /afs/cern.ch/user/l/legao/CMSSW_10_6_31/src/PhysicsTools/MiniAnalysis/scripts/run_filter.sh "$1" "${2/__FLAVOUR__/${PID}}" "$3" "23(${PID},-${PID})" &
 done
 wait
