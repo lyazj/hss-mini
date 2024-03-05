1480047e0512f1b6b0640317b2106df6f0907275
lyazj <seeson@pku.edu.cn>
2024-03-05 13:41:00 +0100

    Add lock for wcb jobs.

--- scripts/run_split_wcb.sh
+++ scripts/run_split_wcb.sh
@@ -5,4 +5,7 @@ if [ $# != 3 ] && [ $# != 4 ]; then
     exit 1
 fi
 
+set -e
+touch "$2".lock
 /afs/cern.ch/user/l/legao/CMSSW_10_6_31/src/PhysicsTools/MiniAnalysis/scripts/run_filter.sh "$1" "$2" "$3" "*24(*4,*5)" "$4"
+rm "$2".lock
