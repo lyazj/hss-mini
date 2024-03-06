7f21383b432368f919c0ee0ccdc2d74948227926
lyazj <seeson@pku.edu.cn>
2024-03-06 05:07:01 +0100

    Fix error handling of run_split_wcb.

--- scripts/run_split_wcb.sh
+++ scripts/run_split_wcb.sh
@@ -10,6 +10,7 @@ LOCKDIR=/afs/cern.ch/user/l/legao/CMSSW_10_6_31/src/PhysicsTools/MiniAnalysis/sc
 LOCKNAME="$(sed 's@/@_@g' <<< "$2")"
 LOCK="${LOCKDIR}"/"${LOCKNAME}"
 mkdir -p "${LOCKDIR}"
+mkdir "${LOCK}" 2>/dev/null || rm "$(sed 's@^root://[^/]*/@@g' <<< "$2")"
 mkdir -p "${LOCK}"
 /afs/cern.ch/user/l/legao/CMSSW_10_6_31/src/PhysicsTools/MiniAnalysis/scripts/run_filter.sh "$1" "$2" "$3" "*24(*4,*5)" "$4"
 rmdir "${LOCK}"
